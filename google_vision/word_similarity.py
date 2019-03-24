fd16457e9791b980048d8a4ea752c6283f5f8b7c
Dray Farley <cf446@cornell.edu>
2019-03-24 14:34:34 -0400

    more wl tests

--- google_vision/word_similarity.py
+++ google_vision/word_similarity.py
@@ -9,10 +9,14 @@ substitute_costs[ord('S'), ord('5')] = 1.0
 substitute_costs[ord('O'), ord('0')] = 1.0
 
 
-print (lev('DISPO', 'DISPO', substitute_costs=substitute_costs))
-print (lev('DISPO', 'DAMN', substitute_costs=substitute_costs))
-print (lev('DISPO', 'D1SPO', substitute_costs=substitute_costs))  # now it prints '1.25'
-print (lev('DISPO', 'D15PO', substitute_costs=substitute_costs))
-print (lev('DISPO', 'D15P0', substitute_costs=substitute_costs))
 
-print (lev('NAM 001', 'NAM OO1', substitute_costs=substitute_costs))
+print ("DISPO and DISPO "+ str(lev('DISPO', 'DISPO', substitute_costs=substitute_costs)))
+print ("DISPO and DAMN "+str(lev('DISPO', 'DAMN', substitute_costs=substitute_costs)))
+print ("DISPO and D1SPO "+str(lev('DISPO', 'D1SPO', substitute_costs=substitute_costs))) 
+print ("DISPO and D15PO "+str(lev('DISPO', 'D15PO', substitute_costs=substitute_costs)))
+print ("DISPO and D15P0 "+str(lev('DISPO', 'D15P0', substitute_costs=substitute_costs)))
+
+print ("NAM 001 and NAM OO1 "+str(lev('NAM 001', 'NAM OO1', substitute_costs=substitute_costs)))
+
+print ("COURT and C0URT "+str(lev('COURT', 'C0URT', substitute_costs=substitute_costs)))
+
