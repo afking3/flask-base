a9fb4e2c85197290cc417194e5c4258a8f1e3995
guowy433110 <1540778560@qq.com>
2019-04-07 15:08:00 -0400

    added routes and example output

--- upload.py
+++ upload.py
@@ -1,4 +1,5 @@
 import os
+import json
 import flask
 from flask import Flask, flash, request, redirect, url_for, render_template, send_from_directory, make_response
 from werkzeug.utils import secure_filename
@@ -9,6 +10,29 @@ ALLOWED_EXTENSIONS = set(['pdf'])
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+crimes = [
+    {
+        "id": 1,
+        "expungement": "felony",
+        "name": "Tom",
+        "location": "Los Angeles",
+        "date": "2018-01-01"
+    },
+    {
+        "id": 2,
+        "expungement": "felony",
+        "name": "Jerry",
+        "location": "Los Angeles",
+        "date": "2018-02-02"
+    },
+    {
+        "id": 3,
+        "expungement": "infraction",
+        "name": "Spike",
+        "location": "Los Angeles",
+        "date": "2018-03-03"
+    }
+]
 
 
 def allowed_file(filename):
@@ -25,7 +49,13 @@ def index():
 #     user = {'username': 'Miguel'}
 #     return render_template('index.html', title='Home', user=user)
 
+@app.route('/review', methods=['GET', 'POST'])
+def review():
+    return render_template("step2.html", data=crimes, back="/upload", next="/download")
 
+@app.route('/download')
+def download():
+    return render_template("step3.html", data=crimes, back="/review", next="/download")
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
@@ -45,15 +75,18 @@ def upload_file():
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             return redirect(url_for('show',
                                     filename=filename))
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
+    return render_template("upload.html", back="/", next="/review")
+    # '''
+    # <!doctype html>
+    # <title>Upload new File</title>
+    # <h1>Upload new File</h1>
+    # <form method=post enctype=multipart/form-data>
+    #   <input type=file name=file>
+    #   <input type=submit value=Upload>
+    # </form>
+    # '''
+
+
 
 
 
@@ -71,12 +104,12 @@ def upload_file():
 # def show_pdf(id=None):
 #     if id is not None:
 #         return render_template('doc.html', doc_id=id)
-@app.route('/show/<filename>')
+@app.route('/show/<filename>', methods=['GET', 'POST'])
 def show(filename):
 #     filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
 #     print(os.listdir(app.config['UPLOAD_FOLDER']))
     # filepath = app.config['UPLOAD_FOLDER'] + "/"+ filename
-    return render_template('step1.html', filename=filename)
+    return render_template('step1.html', reupload="/upload", back="/", next="/review", filename=filename)
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
