71b8f1f8fee59459c886e66773ce10ebc3b8e8e3
zackwitz <zackwitz@gmail.com>
2019-04-24 20:11:50 -0400

    Fixed main to run and export to excel

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
-import ruleset
+import ruleset as rs
 #import vision
-import xlwt 
+import xlwt
 from xlwt import Workbook
 
-''' 
+'''
 Outputs a tuple of the rapsheet and results
     i.e.
         def __init__(self, crime_type, result, convict_date, offense, offense_code, prob_status):
@@ -28,8 +28,8 @@ def getOutputFromRapsheet(rap):
     #return (rapsheet, rapsheet_results)
     pass
 
-''' 
-Given an input (rapsheet, results), 
+'''
+Given an input (rapsheet, results),
 will give an output of the format
 
     crimes = [
@@ -46,69 +46,69 @@ def formatOutput(_input):
     final_crimes = []
     for index in range(len(crimes)):
         crime = crimes[index]
+        print(crime)
         new_obj = {}
-        new_obj.crime_type = crime.crime_type
-        new_obj.result = crime.result
-        new_obj.convict_date = crime.convict_date
-        new_obj.offense = crime.offense
-        new_obj.offense_code = crime.offense_code
-        new_obj.prob_status = crime.prob_status
+        new_obj["crime_type"] = crime.crime_type
+        new_obj["result"] = crime.result
+        new_obj["convict_date"] = crime.conviction_date
+        new_obj["offense_code"] = crime.offense_code
+        new_obj["prob_status"] = crime.probation_status
         result = results[index]
-        new_obj.expunge_result = result[1]
-        new_obj.expunge_messages = result[0]
+        new_obj["expunge_result"] = result[1]
+        new_obj["expunge_messages"] = result[0]
         final_crimes.append(new_obj)
     return final_crimes
 
 '''
-Given an formatted input [input], 
+Given an formatted input [input],
 will create an excel sheet
 from this at the path [path] with the name [filename]
 
-On success, returns the file path. On failure, 
-returns None. 
+On success, returns the file path. On failure,
+returns None.
 '''
 def createExcelSheet(output, filename, path):
-    workbook = xlwt.Workbook()  
-    
-    sheet = workbook.add_sheet("Test") 
-    
-    # Specifying style 
-    style = xlwt.easyxf('font: bold 1') 
-    
-    headers = ["Crime Type", "Result", "Convict Date", "Offense", "Offense Code", "Probation Status", "Expungement Result", "Expungment Messages"]
+    workbook = xlwt.Workbook()
+
+    sheet = workbook.add_sheet("Test")
+
+    # Specifying style
+    style = xlwt.easyxf('font: bold 1')
+
+    headers = ["Crime Type", "Result", "Convict Date", "Offense Code", "Probation Status", "Expungement Result", "Expungment Messages"]
 
     assert len(output) > 0
     assert len(headers) == len(output[0])
 
     for i in range(len(headers)):
         header = headers[i]
-        sheet.write(i, 0, header, style) 
+        sheet.write(0, i, header, style)
 
     next_row = 1
     for crime in output:
-        sheet.write(0, next_row, crime.crime_type, style)
-        sheet.write(1, next_row, crime.result, style)
-        sheet.write(2, next_row, crime.convict_date, style)
-        sheet.write(3, next_row, crime.offense, style)
-        sheet.write(4, next_row, crime.offense_code, style)
-        sheet.write(5, next_row, crime.prob_status, style)
-        sheet.write(6, next_row, crime.expunge_result, style)
-        sheet.write(7, next_row, crime.expunge_messages, style)
-        next_row = new_row + 1
-        
-    # Writting on specified sheet 
+        sheet.write(next_row, 0,  crime["crime_type"], style)
+        sheet.write(next_row, 1, crime["result"], style)
+        sheet.write(next_row, 2, crime["convict_date"], style)
+        sheet.write(next_row, 3, crime["offense_code"], style)
+        sheet.write(next_row, 4, crime["prob_status"], style)
+        sheet.write(next_row, 5, crime["expunge_result"], style)
+        sheet.write(next_row, 6, crime["expunge_messages"], style)
+        next_row = next_row + 1
+
+    # Writting on specified sheet
     if(path[-1] != "/"):
         path = path + "/"
     workbook.save(path + filename)
+    return path + filename
 
 if __name__ == "__main__":
-    given = rs.Rapsheet(
+    rap = rs.Rapsheet(
         [
-            rs.Crime("Felony", "County Jail", week_ago, None, "Not Completed", True),
+            rs.Crime("Felony", "County Jail", "empty", None, "Not Completed", True),
         ])
-    expected = [
-        [[COUNTY_JAIL_DISC], "Discretionary"],
+    res = [
+        [["dis"], "Discretionary"],
     ]
-    x = formatOutput((given, expected))
-    path = createExcelSheet(x, "test.xls", "tests")
-    print path
+    x = formatOutput((rap, res))
+    path = createExcelSheet(x, "test2.xls", "tests")
+    print(path)
