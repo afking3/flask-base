c7ad5afadbefbb31ab2c89026c7bb0d0c9deac93
Dray Farley <cf446@cornell.edu>
2019-05-05 15:02:51 -0400

    new dir

--- app/static/scripts/fullpreview.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $(".view").click(function () {
-    $(".modal").removeClass("hidden");
-    });
-    $(".modal").click(function() {
-      $(".modal").addClass("hidden");
-    });
-});
