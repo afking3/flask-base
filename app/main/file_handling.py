b61eaa1581b3581adee765ed83621fdab053a1d8
Dray Farley <cf446@cornell.edu>
2019-05-20 09:17:50 -0400

    Print dir in file_handling.py

--- app/main/file_handling.py
+++ app/main/file_handling.py
@@ -18,7 +18,10 @@ def _open_single_image(filename):
 Opens a PDF and converts to a list of PIL images.
 """
 def _open_pdf(filename):
+    os.listdir("app/main/uploads/")
+    print("******")
     print("app/main/uploads/"+filename)
+    print("******")
     imgs = pdf2image.convert_from_path("app/main/uploads/"+filename)
     map((lambda i: i.convert), imgs)
     return imgs
